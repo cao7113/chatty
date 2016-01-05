bc376f8dbd160a882ace92f6705fc6764b46526f
caoruijian <cao7113@hotmail.com>
2016-01-05 12:27:11 +0800

    some customization

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,6 @@
 # create default test user
-User.create!(name: "Chatty Bot", email: "chattybot@example.com", password: "12345", password_confirmation: "12345")
+#User.create!(name: "Chatty Bot", email: "chattybot@example.com", password: "12345", password_confirmation: "12345")
+
+%w(dev1 dev2 dev3 test1 test2 test3).each do |uname|
+  User.create!(name: uname, email: "#{uname}@dev.com", password: "123456", password_confirmation: "123456")
+end
